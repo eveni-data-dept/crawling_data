fc8310bef53c3a6971bed3021aaf05d7f6409eb0
eveni-data-dept <106415360+eveni-data-dept@users.noreply.github.com>
2022-06-04 11:19:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,55 +1,51 @@
 import gspread
-
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+
 scope = [
-'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive',
 ]
-json_file_name = 'eveni-database-e469f5582c7f.json'
+
+json_file_name = 'eveni-database-4ecde3632479.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
 gc = gspread.authorize(credentials).open('[eveni] 지원팀 데이터')
-# spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1p2LpwzwqQYmyNkCEv6lcVLDjOAbr-TnaQZEnoh7-VwA/edit#gid=1048343811'
-# doc = gc.open_by_url(spreadsheet_url)
-#worksheet = doc.worksheet('시트3')
-# range_list = worksheet.range('{범위, 예시 "A1:B1"}')
-#     for cell in range_list:
-#         values[i].append(cell.value)
+
+
+
+
+#https://console.cloud.google.com/iam-admin/serviceaccounts?hl=ko&project=프로젝트명
+#key 발급 -> 
 
 data= gc.worksheet('시트3')
 records_data = data.get_all_records()
-
-import pandas as pd
 records_df = pd.DataFrame.from_dict(records_data)
 
-# view the top records
-records_df.head()
-
-from datalab.context import Context
-import datalab.storage as storage
-import datalab.bigquery as bq
 import pandas as pd
-from pandas import DataFrame
-import time
+import numpy as np
+records_df = pd.DataFrame.from_dict(records_data)
 
 
-#Alternative 3
-start = time.time()
-sample_bucket_name = Context.default().project_id + '-datalab-example'
-sample_bucket_path = 'gs://' + sample_bucket_name
-sample_bucket_object = sample_bucket_path + '/Hello.txt'
-bigquery_dataset_name = 'TestDataSet'
-bigquery_table_name = 'TestTable'
+records_df["시간"] = records_df["시간"].astype(str)
+records_df = records_df.replace('', np.NaN)
+# view the top records
+records_df.rename(columns = {'컨설팅일자':'class_date', '시간':'class_time', '컨설턴트':'consultant', '고객명':'student','예정/완료':'is_done'}, inplace = True)
 
-# Define storage bucket
-sample_bucket = storage.Bucket(sample_bucket_name)
 
-# Create or overwrite the existing table if it exists
-table_schema = bq.Schema.from_dataframe(not_so_simple_dataframe)
+credentials = service_account.Credentials.from_service_account_file(json_file_name)
+records_df.to_gbq('eveni.test', project_id='eveni-database, chunksize=None, reauth=False , if_exists='fail', credentials = credentials)
 
-# Write the DataFrame to GCS (Google Cloud Storage)
-%storage write --variable not_so_simple_dataframe --object $sample_bucket_object
+#query = "SELECT * FROM `dataset.table`"
+#df = pd.read_gbq(query=query, project_id="project_id", credentials=credentials, dialect='standard')
+# 'fail'
+# If table exists raise pandas_gbq.gbq.TableCreationError.
 
-# Write the DataFrame to a BigQuery table
-table.insert_data(not_so_simple_dataframe)
-end = time.time()
-print("time alternative 3 " + str(end - start))
+# 'replace'
+# If table exists, drop it, recreate it, and insert data.
+
+# 'append'
+# If table exists, insert data. Create if does not exist.
+                  
